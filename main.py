05b0f111fc60802c8dadacc4d6bc39bf00c54c94
ai-team-sg <ai-team@studentgator.com>
2025-12-11 11:20:48 +0200

    fixing the import error

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os 
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.general_search import search
